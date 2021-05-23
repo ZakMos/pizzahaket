a26c532b3a64bb46bc5e133361519346939b2dae
zakmos <z.zza@hotmail.com>
2021-05-23 08:53:12 +0200

    components with views

---
+++ src/views/Main.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap';
+
+const Main = () => {
+    return (
+        <>
+        <Container>
+            <Row>
+             <Col>This is Main</Col>
+             </Row>
+        </Container>
+        </>
+    )
+}
+
+export default Main;
